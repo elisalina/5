bc7b950416e44ae30792f11df55c5a71621c47cb
Алина Демчук <alina2805@bk.ru>
2024-06-25 12:57:51 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+#immutable_var = (1, 2, "Alina")
+#print(immutable_var)
+#immutable_var [0] = 3
+#print(immutable_var)
+#ошибка, так как нельзя заменять элементы кортежа
+mutable_list = [2, 4, "23", "Dog"]
+mutable_list[0] = 3
+print(mutable_list)
